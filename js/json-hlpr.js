4a635d813bdcec71282c7ff1d8dde73c695b05b0
rnbwspctrm <bluewhirligig@gmail.com>
2016-05-16 12:13:05 -0700

    Added json-hlpr file.

---
+++ js/json-hlpr.js
@@ -0,0 +1,35 @@
+(function() {
+	var ein = ECO_INT_NAMESPACE;
+	ein.jsonHlpr = {
+		save: buildJsonFile
+	};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function buildJsonFile(validatedRcrds) {
+	console.log("buildJsonFile called. rcrds = %O", validatedRcrds);
+}
+
+
+
+
+
+
+
+
+
+}()); /* End of namespacing anonymous function */
